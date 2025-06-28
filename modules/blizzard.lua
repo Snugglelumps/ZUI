43b001eff264557cfad47205946c60aef89c0eeb
Snugglelumps <z.a.fisher@gmail.com>
2025-06-28 17:58:19 -0400

    fixed namespace in blizzard.lua

--- modules/blizzard.lua
+++ modules/blizzard.lua
@@ -1,12 +1,13 @@
+local _, zui = ...
 local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_LOGIN")
 f:SetScript("OnEvent", function()
     -- Hook the default tooltip anchor behavior
-    hooksecurefunc("GameTooltip_SetDefaultAnchor", function(tooltip, parent)
-        if rightAnchor and tooltip then
+    hooksecurefunc("GameTooltip_SetDefaultAnchor", function(tooltip)
+        if zui.frames.rightAnchor and tooltip then
             tooltip:ClearAllPoints()
             tooltip:SetOwner(UIParent, "ANCHOR_NONE")
-            tooltip:SetPoint("TOPRIGHT", rightAnchor, "TOPLEFT", -10, 0)
+            tooltip:SetPoint("TOPRIGHT", zui.frames.rightAnchor, "TOPLEFT", -10, 0)
         end
     end)
 end)
